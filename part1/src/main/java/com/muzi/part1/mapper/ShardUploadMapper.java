30be52268a260ce8e1a32020addb98c9f557670a
木子金又二丨 <74045656+MuziGeek@users.noreply.github.com>
2024-04-18 17:28:20 +0800

    Delete part1 directory

--- part1/src/main/java/com/muzi/part1/mapper/ShardUploadMapper.java
+++
@@ -1,8 +0,0 @@
-package com.muzi.part1.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.muzi.part1.po.ShardUploadPO;
-
-
-public interface ShardUploadMapper extends BaseMapper<ShardUploadPO> {
-}
--- part1/src/main/java/com/muzi/part1/mapper/ShardUploadPartMapper.java
+++
@@ -1,8 +0,0 @@
-package com.muzi.part1.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.muzi.part1.po.ShardUploadPartPO;
-
-
-public interface ShardUploadPartMapper extends BaseMapper<ShardUploadPartPO> {
-}
